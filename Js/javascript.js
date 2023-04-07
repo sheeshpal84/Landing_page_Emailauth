10d026d8f629d2ea9e68cc127ae20a7d9df3346f
sheeshpal kotwal <sheeshpalkotwal@gmail.com>
2023-04-07 11:41:16 +0530

    first commit

---
+++ Js/javascript.js
@@ -0,0 +1,6 @@
+// -----Country Code Selection
+$("#mobile_code").intlTelInput({
+	initialCountry: "in",
+	separateDialCode: true,
+	// utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.4/js/utils.js"
+});
